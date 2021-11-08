1af845698946232ea64759442d71d344511ef606
Matt Coraccio <mdcoraccio@gmail.com>
2021-11-08 12:17:14 +0000

    updates to the bot, whatchu want from me

--- commands/msc.js
+++ commands/msc.js
@@ -1,103 +1,106 @@
-const {SlashCommandBuilder} = require('@discordjs/builders')
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const sql = require('mssql')
-const {config} = require('../sql_config');
+const { config } = require('../sql_config');
 const axios = require('axios')
+const msc = 1; // MSC is #1!
 
 const commands = [
-    {
-        data: new SlashCommandBuilder()
-        .setName('tlmsc')
-        .setDescription('MSC Character Tier Rankings'),
+	{
+		data: new SlashCommandBuilder()
+			.setName('tlmsc')
+			.setDescription('MSC Character Tier Rankings'),
 
-        async execute(interaction) {
-            interaction.reply("https://media.discordapp.net/attachments/806813942218883073/869189371847381022/unknown.png");
-            interaction.followUp("https://media.discordapp.net/attachments/806813942218883073/869189471533400074/unknown.png");
-        }
-    },
-    {
-        data: new SlashCommandBuilder()
-        .setName('mscrating')
-        .setDescription('Current Ratings for All Competitive MSC Players'),
-
-        async execute(interaction) {
-            try {
-                sql.connect(config,(err) => {
-                    const request = new sql.Request();
-                    const query = 'exec GetRatingsForDiscordMSC';
-                    request.query(query, (err, recordset) => {
-                        if (err) {
-                            console.log(err);
-                            interaction.reply('❌')
-                        } else {
-                            let mscRatings = ''
-                            recordset.recordset.forEach((record) => {
-                                mscRatings +=`${record.line}\n`
-                            })
-                            interaction.reply(mscRatings)
-                        }
-                    });
-                });
-            } catch (error) {
-                console.log(error);
-                interaction.reply('❌')
-            }
-        }
-    },
-    {
-        data: new SlashCommandBuilder()
-        .setName('mslmsc')
-        .setDescription('MSL MSC Rankings'),
-
-        async execute(interaction) {
-            const url3 = "https://docs.google.com/spreadsheets/d/1Cf5YggxcwNVMCTyQ1Xz7wC7OaOxVoC1rQIeEJSXo6s8/gviz/tq?";
-            interaction.reply("**MSL Season 1** — MSC Rankings");
-			axios.get(url3)
-			.then(function (response) {
-				const data = JSON.parse(response.data.substr(47).slice(0,-2));
-				const newData3 = [];
-
-				data.table.rows.map((main)=>{
-					newData3.push(main.c[0].v);
-                })
-				interaction.followUp(newData3.join('\n'));
-				// I need this data here ^^
-				return response.data;
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
+		async execute(interaction) {
+			interaction.reply("https://media.discordapp.net/attachments/806813942218883073/869189371847381022/unknown.png");
+			interaction.followUp("https://media.discordapp.net/attachments/806813942218883073/869189471533400074/unknown.png");
 		}
-    },
-    {
-        data: new SlashCommandBuilder()
-        .setName('mscreport')
-        .setDescription('Reporting MSC Matches')
-        .addUserOption(option => option.setName('p1').setDescription('Player 1'))
-        .addUserOption(option => option.setName('p2').setDescription('Player 2'))
-        .addStringOption(option => option.setName('score').setDescription('Score'))
-        .setDefaultPermission(false),
+	},
+	{
+		data: new SlashCommandBuilder()
+			.setName('mscrating')
+			.setDescription('Current Ratings for All Competitive MSC Players'),
 
-        async execute(interaction) {
-            try {
-               const p1 = interaction.options.getUser('p1').username;
-               const p2 = interaction.options.getUser('p2').username;
-               sql.connect(config, (err) => {
-                    const request = new sql.Request();
-                    const query = "exec reportScoreMSC @p1, @p2, @score;"
-                    request.input("p1", p1);
-                    request.input("p2", p2);
-                    request.input("score", interaction.options.getString('score'));
-                    request.query(query, (err,recordset) => {
-                       if (err) console.log(err)
-                    })
-                })
-            interaction.reply('☑️') 
-            } catch(error) {
-                console.log(error)
-                interaction.reply('❌')
-            }
-        }
-    }
+		async execute(interaction) {
+			try {
+				sql.connect(config, (err) => {
+					const request = new sql.Request();
+					const query = 'exec GetRatingsForDiscord @gametype';
+					request.input('gametype', msc);
+					request.query(query, (err, recordset) => {
+						if (err) {
+							console.log(err);
+							interaction.reply('❌')
+						} else {
+							let mscRatings = ''
+							recordset.recordset.forEach((record) => {
+								mscRatings += `${record.line}\n`
+							})
+							interaction.reply(mscRatings)
+						}
+					});
+				});
+			} catch (error) {
+				console.log(error);
+				interaction.reply('❌')
+			}
+		}
+	},
+	{
+		data: new SlashCommandBuilder()
+			.setName('mslmsc')
+			.setDescription('MSL MSC Rankings'),
+
+		async execute(interaction) {
+			const url3 = "https://docs.google.com/spreadsheets/d/1Cf5YggxcwNVMCTyQ1Xz7wC7OaOxVoC1rQIeEJSXo6s8/gviz/tq?";
+			interaction.reply("**MSL Season 1** — MSC Rankings");
+			axios.get(url3)
+				.then(function (response) {
+					const data = JSON.parse(response.data.substr(47).slice(0, -2));
+					const newData3 = [];
+
+					data.table.rows.map((main) => {
+						newData3.push(main.c[0].v);
+					})
+					interaction.followUp(newData3.join('\n'));
+					// I need this data here ^^
+					return response.data;
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		}
+	},
+	{
+		data: new SlashCommandBuilder()
+			.setName('mscreport')
+			.setDescription('Reporting MSC Matches')
+			.addUserOption(option => option.setName('p1').setDescription('Player 1'))
+			.addUserOption(option => option.setName('p2').setDescription('Player 2'))
+			.addStringOption(option => option.setName('score').setDescription('Score'))
+			.setDefaultPermission(false),
+
+		async execute(interaction) {
+			try {
+				const p1 = interaction.options.getUser('p1').username;
+				const p2 = interaction.options.getUser('p2').username;
+				sql.connect(config, (err) => {
+					const request = new sql.Request();
+					const query = "exec reportScoreMSC @gametype, @p1, @p2, @score;"
+					request.input("gametype", msc);
+					request.input("p1", p1);
+					request.input("p2", p2);
+					request.input("score", interaction.options.getString('score'));
+					request.query(query, (err, recordset) => {
+						if (err) console.log(err)
+					})
+				})
+				interaction.reply('☑️')
+			} catch (error) {
+				console.log(error)
+				interaction.reply('❌')
+			}
+		}
+	}
 ]
 
-module.exports = {commands}
+module.exports = { commands }
--- commands/sms.js
+++ commands/sms.js
@@ -1,104 +1,107 @@
-const {SlashCommandBuilder} = require('@discordjs/builders')
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const sql = require('mssql')
-const {config} = require('../sql_config');
+const { config } = require('../sql_config');
 const axios = require('axios')
-const rules = ["Test Rule 1", "Test Rule 2", "Test Rule 3"]
+const sms = 2; // because SMS is shit! :P
+const rules = ['Items Off', 'Bowser Attack On', 'Everything On', 'Normal Competitive Rules', 'First to 10 in Strikers 101', 'Infinite Items', 'Giant Items', 'No Toads', 'Create Your Own Rules', 'Superstrikes On']
 const commands = [
-    {
-        data: new SlashCommandBuilder()
-        .setName('smsrating')
-        .setDescription('Current Ratings for All Competitive SMS Players'),
+	{
+		data: new SlashCommandBuilder()
+			.setName('smsrating')
+			.setDescription('Current Ratings for All Competitive SMS Players'),
 
-        async execute(interaction) {
-            try {
-                sql.connect(config,(err) => {
-                    const request = new sql.Request();
-                    const query = 'exec GetRatingsForDiscordSMS';
-                    request.query(query, (err, recordset) => {
-                        if (err) {
-                            console.log(err);
-                            interaction.reply('❌')
-                        } else {
-                            let smsRatings = ''
-                            recordset.recordset.forEach((record) => {
-                                smsRatings +=`${record.line}\n`
-                            })
-                            interaction.reply(smsRatings)
-                        }
-                    });
-                });
-            } catch (error) {
-                console.log(error);
-                interaction.reply('❌')
-            }
-        }
-    },
-    {
-        data: new SlashCommandBuilder()
-        .setName('mslsms')
-        .setDescription('SMS MSL Rankings'),
-
-        async execute(interaction) {
-            const url4 = "https://docs.google.com/spreadsheets/d/1elh4wTVHNR0dv-QNklaNLMUNU07VKuaFkZkU9G2XWQ0/gviz/tq?";
-			
-            interaction.reply("**MSL Season 1** — SMS Rankings");
-			axios.get(url4)
-			.then(function (response) {
-				const data = JSON.parse(response.data.substr(47).slice(0,-2));
-				const newData4 = [];
-
-				data.table.rows.map((main)=>{
-					newData4.push(main.c[0].v);
-                })
-				interaction.followUp(newData4.join('\n'));
-				// I need this data here ^^
-				return response.data;
-			})
-			.catch(function (error) {
+		async execute(interaction) {
+			try {
+				sql.connect(config, (err) => {
+					const request = new sql.Request();
+					const query = 'exec GetRatingsForDiscord @gametype';
+					request.input('gametype', sms);
+					request.query(query, (err, recordset) => {
+						if (err) {
+							console.log(err);
+							interaction.reply('❌')
+						} else {
+							let smsRatings = ''
+							recordset.recordset.forEach((record) => {
+								smsRatings += `${record.line}\n`
+							})
+							interaction.reply(smsRatings)
+						}
+					});
+				});
+			} catch (error) {
 				console.log(error);
-			});
+				interaction.reply('❌')
+			}
 		}
-    },
-    {
-        data: new SlashCommandBuilder()
-        .setName('smsreport')
-        .setDescription('Reporting SMS Matches')
-        .addUserOption(option => option.setName('p1').setDescription('Player 1'))
-        .addUserOption(option => option.setName('p2').setDescription('Player 2'))
-        .addStringOption(option => option.setName('score').setDescription('Score'))
-        .setDefaultPermission(false),
+	},
+	{
+		data: new SlashCommandBuilder()
+			.setName('mslsms')
+			.setDescription('SMS MSL Rankings'),
 
-        async execute(interaction) {
-            try {
-               const p1 = interaction.options.getUser('p1').username;
-               const p2 = interaction.options.getUser('p2').username;
-               sql.connect(config, (err) => {
-                    const request = new sql.Request();
-                    const query = "exec reportScoreSMS @p1, @p2, @score;"
-                    request.input("p1", p1);
-                    request.input("p2", p2);
-                    request.input("score", interaction.options.getString('score'));
-                    request.query(query, (err,recordset) => {
-                       if (err) console.log(err)
-                    })
-                })
-            interaction.reply('☑️') 
-            } catch(error) {
-                console.log(error)
-                interaction.reply('❌')
-            }
-        }
-    },
-    {
-        data: new SlashCommandBuilder()
-            .setName('randomrule')
-            .setDescription('Generate a random rule for SMS matches')
-            .setDefaultPermission(true),
+		async execute(interaction) {
+			const url4 = "https://docs.google.com/spreadsheets/d/1elh4wTVHNR0dv-QNklaNLMUNU07VKuaFkZkU9G2XWQ0/gviz/tq?";
 
-        async execute(interaction) {
-            interaction.reply(rules[Math.floor(Math.random() * rules.length)]);
-        }
-    }
+			interaction.reply("**MSL Season 1** — SMS Rankings");
+			axios.get(url4)
+				.then(function (response) {
+					const data = JSON.parse(response.data.substr(47).slice(0, -2));
+					const newData4 = [];
+
+					data.table.rows.map((main) => {
+						newData4.push(main.c[0].v);
+					})
+					interaction.followUp(newData4.join('\n'));
+					// I need this data here ^^
+					return response.data;
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		}
+	},
+	{
+		data: new SlashCommandBuilder()
+			.setName('smsreport')
+			.setDescription('Reporting SMS Matches')
+			.addUserOption(option => option.setName('p1').setDescription('Player 1'))
+			.addUserOption(option => option.setName('p2').setDescription('Player 2'))
+			.addStringOption(option => option.setName('score').setDescription('Score'))
+			.setDefaultPermission(false),
+
+		async execute(interaction) {
+			try {
+				const p1 = interaction.options.getUser('p1').username;
+				const p2 = interaction.options.getUser('p2').username;
+				sql.connect(config, (err) => {
+					const request = new sql.Request();
+					const query = "exec reportScoreSMS @gametype, @p1, @p2, @score;"
+					request.input("gametype", sms);
+					request.input("p1", p1);
+					request.input("p2", p2);
+					request.input("score", interaction.options.getString('score'));
+					request.query(query, (err, recordset) => {
+						if (err) console.log(err)
+					})
+				})
+				interaction.reply('☑️')
+			} catch (error) {
+				console.log(error)
+				interaction.reply('❌')
+			}
+		}
+	},
+	{
+		data: new SlashCommandBuilder()
+			.setName('randomrule')
+			.setDescription('Generate a random rule for SMS matches')
+			.setDefaultPermission(true),
+
+		async execute(interaction) {
+			interaction.reply(rules[Math.floor(Math.random() * rules.length)]);
+		}
+	}
 ]
 
-module.exports = {commands}
+module.exports = { commands }
