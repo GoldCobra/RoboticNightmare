251d721089c3e0cdc2ed0e8a76eca1c572b20242
Owen Dreikosen <odreikosen@gmail.com>
2021-12-13 01:46:28 -0500

    Maybe

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-source /home/ec2-user/.bash_profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+npm install -C /home/ec2-user/RoboticNightmare
 npm run start
