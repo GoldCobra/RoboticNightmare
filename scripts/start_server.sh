335a1b61025f0cb4671794fdb2132907485708d0
Owen <odreikosen@gmail.com>
2021-12-13 02:20:36 -0500

    Node install for root

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+cd /home/ec2-user/RoboticNightmare
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v16.6.0
 npm install -C /home/ec2-user/RoboticNightmare
 npm run start
