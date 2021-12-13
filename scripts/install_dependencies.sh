828be14580ac62eb32e9da07671c3cfd20920d83
Owen Dreikosen <odreikosen@gmail.com>
2021-12-13 00:57:10 -0500

    more

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-npm install
+export $WORKDIR
+npm install -C /home/ec2-user/RoboticNightmare
