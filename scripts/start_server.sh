8ac678aad0494756a5057e3ad78df422ee71aacb
Owen <odreikosen@gmail.com>
2021-12-13 02:43:59 -0500

    background

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ cd /home/ec2-user/RoboticNightmare
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v16.6.0
-npm install -C /home/ec2-user/RoboticNightmare
-npm run start
+npm install /home/ec2-user/RoboticNightmare
+npm run start > /dev/null 2> /dev/null < /dev/null &
