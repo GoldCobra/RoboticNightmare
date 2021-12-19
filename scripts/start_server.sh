1a835fa66c9e3d4ed1f5d0f81ba19143e301ccbd
Owen <odreikosen@gmail.com>
2021-12-19 13:12:04 -0500

    Npm binary fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v16.6.0
 npm install /home/ec2-user/RoboticNightmare
-npm run start > /dev/null 2> /dev/null < /dev/null &
+node index.js > /dev/null 2> /dev/null < /dev/null &
