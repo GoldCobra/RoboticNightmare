a51661cdb4094c8eb36e119bbbab8ce597447946
Owen <odreikosen@gmail.com>
2022-02-10 22:24:14 -0500

    Adding logging

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,4 +5,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install v16.6.0
 npm install /home/ec2-user/RoboticNightmare
 killall node
-node index.js > /dev/null 2> /dev/null < /dev/null &
+node index.js > /home/ec2-user/RoboticNightmare/app.log 2> /home/ec2-user/RoboticNightmare/app.log < /home/ec2-user/RoboticNightmare/app.log &
